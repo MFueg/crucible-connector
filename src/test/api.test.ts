c0d89be765962fc1d5cdded2a114db835dbcfca7
Matthias Füg <matthias.fueg@dampsoft.de>
2019-10-19 16:12:32 +0200

    - prepared api tests (still empty)

--- src/test/api.test.ts
+++ src/test/api.test.ts
@@ -1,15 +1,25 @@
 import { expect } from 'chai';
-const mockServer = require('mockttp').getLocal();
 import 'mocha';
 // import { ApiCrucible } from '../api';
+import * as mock from 'mockttp'
 
 describe('Hello function', () => {
-  // Start your server
-  beforeEach(() => mockServer.start(8080));
+  const mockServer = mock.getLocal();
+  // const api = new ApiCrucible("https://localhost:443", "admin", "admin", false);
+
+  beforeEach(() => mockServer.start());
   afterEach(() => mockServer.stop());
 
-  it('should return hello world', () => {
-    // let api = new ApiCrucible("localhost", "test", "test", false);
-    expect('Hello World!').to.equal('Hello World!');
+  it('should request an existing repository', async () => {
+    expect(true).is.equals(true);
+    /*
+    const endpointMock = await mockServer.get("/mocked-endpoint").thenReply(200, "hmm?");
+
+    const result = await api.searchRepositories({ types: ["git"] });
+    expect(result).not.undefined;
+
+    const requests = await endpointMock.getSeenRequests();
+    expect(requests.length).to.equal(1);
+    expect(requests[0].url).to.equal(`https://localhost:${mockServer.port}/mocked-endpoint`); */
   });
 });
